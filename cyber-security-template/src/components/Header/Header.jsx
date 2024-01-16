b48788cb51b577699208f35e765f28f753c698ae
Oscar Pacheco Ortiz <Oscarpa9188@gmail.com>
2024-01-15 19:10:14 -0600

    Header Component
    Header Component with navigation
    
    - icons still need to be matching the logo color
    -  navigation is working on the desktop
    
    - need to implement the mobile version of the navbar

--- cyber-security-template/src/components/Header/Header.jsx
+++ cyber-security-template/src/components/Header/Header.jsx
@@ -6,12 +6,15 @@ export default function Header(){
 
     let tabStyle = "transition ease-in-out duration-500 text-gray-800/50 hover:text-gray-800"
 
-    return(
-        <header className="flex flex-row justify-between w-auto items-center px-12 mt-10 items-center">
-            <h1 className="blue-font text-3xl md:text-5xl font-black w-full" >SECUREM4</h1>
+    let border  = "border-2 border-black border-solid";
 
-            <menu className="flex flex-row flex-nowrap w-full mx-auto justify-between font-extrabold text-xl ">
-                
+    return(
+        <header className=" container flex flex-row mx-auto justify-between items-center px-6 py-8 items-center">
+            <h1 className="blue-font text-3xl md:text-5xl font-black w-1/4 " >SECUREM4</h1>
+
+            <div className='flex flex-row items-center'>
+                <menu className="flex flex-row flex-nowrap ml-auto w-1/2 justify-between font-extrabold text-lg ">
+                    
                     <li className={tabStyle}>
                         <a href="#">
                             Home
@@ -27,22 +30,23 @@ export default function Header(){
                         +1 (234) - 567 - 8901
                     </li>
             
-            </menu>
+                </menu> 
 
-            {/* <menu className="flex flex-row flex-nowrap w-auto space-x-2">
+                <menu className="flex flex-row flex-nowrap w-1/4 space-x-2 justify-center">
+                    
+                        <li className=' drop-shadow-xl silver-bg rounded-full  w-3/12 p-5 self-center '>
+                            <img  className="" src={facebook} alt="Facebook page" />
+                        </li>
+                        <li className=' drop-shadow-xl silver-bg rounded-full  w-3/12 self-center p-5'>
+                            <img className='image-2' src={twitter} alt="Twitter page" />
+                        </li >
+                        <li  className=' drop-shadow-xl silver-bg rounded-full  w-3/12 self-center p-5'>
+                            <img  src={instagram} alt="Instagram page" />
+                        </li>
+
                 
-                    <li className='rounded-full border-solid border-2 border-black p-4 w-3/12'>
-                        <img src={facebook} alt="Facebook page" />
-                    </li>
-                    <li className='rounded-full border-solid border-2 border-black p-4 w-3/12'>
-                        <img src={twitter} alt="Twitter page" />
-                    </li >
-                    <li className='rounded-full border-solid border-2 border-black p-4 w-3/12'>
-                        <img src={instagram} alt="Instagram page" />
-                    </li>
-
-            
-            </menu> */}
+                </menu>
+            </div>
       </header>
     )
 }
