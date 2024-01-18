39286675636bb1a10fde414000f10f7237f216d1
cleveroscar <Oscarpa9188@gmail.com>
2024-01-18 13:31:31 -0600

    Update Card.jsx
    Updated Card Component
    
    - responsiveness from mobile to desktop
    - images are now responsive as they should
    - text styles updated

--- cyber-security-template/src/components/Features/Card.jsx
+++ cyber-security-template/src/components/Features/Card.jsx
@@ -4,15 +4,22 @@ import {FEATURES} from './data';
 export default function Card(){
 
     return(
-         <div className='grid grid-cols-1 justify-items-center '>
+         <div className='grid grid-cols-1 sm:gird-cols-2 md:grid-cols-2 lg:grid-cols-3 justify-items-center px-20 md:px-0 md:gap-10 lg:gap-20'>
                 {FEATURES.map(feature => 
-                    <div className='border-2 border-black border-solid w-2/4 rounded-3xl p-4 silver-bg my-10'>
+                    <div className=' w-full rounded-[40px] px-4 py-6 bg-gray-[#f2f3f7] my-10 shadow-2xl'>
                         
-                         <img src={feature.image} alt="feature1"  className=' rounded-3xl' style={{ width: '200px', height: '150px' }}/>
 
-                         <h3 className='py-4 text-3xl purple-text font-bold'> {feature.title}</h3>
+                        <div class="relative max-w-auto px-2 overflow-hidden bg-cover bg-no-repeat rounded-3xl mx-auto">
+                            <img src={feature.image} alt="feature1"  className='transition duration-300 ease-in-out hover:scale-110 rounded-3xl  w-full h-64 object-cover' />
+                        </div>
 
-                         <p>{feature.description}</p>
+                        <div className='p-4 space-y-6'>
+                            <h3 className='py-4 text-3xl purple-text font-bold'> {feature.title}</h3>
+
+                            <p className='silver-text font-semibold text-xl'>{feature.description}</p>
+
+                            <p className='font-bold blue-font text-xl'>Read More</p>
+                        </div>
 
                     </div>
                 )}
