4eaf3c66d8bb8abe6e0a23fac7d5682ddcf99eab
cleveroscar <Oscarpa9188@gmail.com>
2024-01-19 14:32:12 -0600

    Updated About
    About Component
    
    - displaying all three sections
    - making them responsive for mobile and desktop
    - css styles have been applied

--- cyber-security-template/src/components/About/About.jsx
+++ cyber-security-template/src/components/About/About.jsx
@@ -1,25 +1,118 @@
 import computer from '../../assets/features3.jpeg'
+import click from '../../assets/click.png'
+import shield from '../../assets/verified.png'
 
 export default function About(){
+
+    let wrapper = 'w-full h-64 p-10  silver-bg rounded-[50px] shadow-2xl '
+    let wrapperTittle = 'text-4xl purple-text font-bold py-4'
+
     return(
-        <div className="py-20 container mx-auto grid grid-cols-1 md:grid-cols-2 px-6 md:px-0">
-            <div className='py-10 space-y-10'>
-                <p className="text-6xl purple-text font-bold w-3/4">
-                    About Cyber Security 
+       <div className='container px-6 mx-auto py-20'>
+            
+
+            {/* About Cyber Security */}
+            <div className="py-20  mx-auto grid grid-cols-1 md:grid-cols-2 gap-20">
+                <div className='py-10 space-y-10'>
+
+                    <div className='block md:hidden self-center '>
+                        <img className=' rounded-[50px] shadow-2xl' src={computer} alt="computer" />
+                    </div>
+
+                    <p className="text-6xl purple-text font-bold w-3/4">
+                        About Cyber Security 
+                    </p>
+
+                    <p className="silver-text text-xl">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed interdum fringilla lacus eget mattis. Integer blandit rutrum ultricies. In nec turpis finibus, sodales enim id, malesuada dui. Fusce tempor ex non molestie placerat. Nulla pellentesque varius dui, a posuere elit. Donec et tellus magna. Donec ut luctus augue. Pellentesque tortor lectus, ultricies eu ipsum vel, sodales euismod enim.
+                    </p>
+
+                    <button className="bg-[#36b8fc] px-10 py-4 rounded-full text-white font-bold">
+                        Read More
+                    </button>
+                </div>
+
+                <div className='hidden md:block self-center'>
+                    <img className='shadow-2xl rounded-[50px]' src={computer} alt="computer" />
+                </div>
+            </div>
+            {/* Level Of Security */}
+            <div className='py-10 '>
+
+                <p className="text-5xl text-center purple-text font-extrabold w-full pb-10">
+                    The highest level of security!
                 </p>
 
-                <p className="silver-text text-xl">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed interdum fringilla lacus eget mattis. Integer blandit rutrum ultricies. In nec turpis finibus, sodales enim id, malesuada dui. Fusce tempor ex non molestie placerat. Nulla pellentesque varius dui, a posuere elit. Donec et tellus magna. Donec ut luctus augue. Pellentesque tortor lectus, ultricies eu ipsum vel, sodales euismod enim.
-                </p>
+                <div className='grid grid-cols-1 md:grid-cols-2 gap-20 w-fit mx-auto py-10'>
+                    
+                    <div className={wrapper}>
+                        <img className='w-2/12' style={{'color': "#36b8fc"}} src={click} alt="click image" />
+                        <h3 className={wrapperTittle}>4 230 000</h3>
+                        <h4 className='text-lg purple-text font-bold '>known viruses</h4>
+                    </div>
 
-                <button className="bg-[#36b8fc] px-10 py-4 rounded-full text-white font-bold">
-                    Read More
-                </button>
+
+                    <div className={wrapper}>
+                        <img className='w-2/12' src={shield}  alt="shield image"/>
+                        <h3 className={wrapperTittle}>99.8%</h3>
+                        <h4 className='text-lg purple-text font-bold'>viruses blokced</h4>
+                    </div>
+                </div>
+
             </div>
 
-            <div>
-                <img className='rounded-[50px]' src={computer} alt="computer" />
+            {/* Secure Features */}
+            <div className='grid grid-cols-1 md:grid-cols-2 gap-20 py-20 items-center'>
+
+                <img className=' max-h-96  w-full rounded-[50px]' src={computer} />
+                <div className='py-10 '>
+                    <h3 className='text-5xl font-bold purple-text'>Secure Features</h3 >
+                    <p className='text-xl py-10 font-bold silver-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elitdo eiusmod temporeum dicant partem scripserit.</p>
+
+                    <ul className='space-y-8 '>
+                        <li className='text-xl font-bold silver-text'>
+                            <div className='flex flex-row justify-between'>
+                                <p>
+                                    Smart Home Security
+                                </p>
+                                <p className='silver-text'>
+                                    92%
+                                </p>
+                            </div>
+                            <div class="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
+                                <div class="bg-[#36197d] h-2.5 rounded-full" style={{width: "92%"}}></div>
+                            </div>
+                        </li>
+
+                        <li className='text-xl font-bold silver-text'>
+                            <div className='flex flex-row justify-between'>
+                                <p>
+                                    Cyber Security
+                                </p>
+                                <p className='silver-text'>
+                                    71%
+                                </p>
+                            </div>
+                            <div class="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
+                                <div class="bg-[#36197d] h-2.5 rounded-full" style={{width: "71%"}}></div>
+                            </div>
+                        </li>
+                        <li className='text-xl font-bold silver-text'>
+                             <div className='flex flex-row justify-between'>
+                                <p>
+                                    CCTV Cameras
+                                </p>
+                                <p className='silver-text'>
+                                    53%
+                                </p>
+                            </div>
+                            <div class="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
+                                <div class="bg-[#36197d] h-2.5 rounded-full" style={{width: "53%"}}></div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
             </div>
-        </div>
+       </div>
     )
 }
--- cyber-security-template/src/components/Features/Card.jsx
+++ cyber-security-template/src/components/Features/Card.jsx
@@ -4,7 +4,7 @@ import {FEATURES} from './data';
 export default function Card(){
 
     return(
-         <div className='grid grid-cols-1 sm:gird-cols-2 md:grid-cols-2 lg:grid-cols-3 justify-items-center px-20 md:px-0 md:gap-10 lg:gap-20'>
+         <div className='grid grid-cols-1 sm:gird-cols-2 md:grid-cols-2 lg:grid-cols-3 justify-items-center px-10 md:px-20 md:px-0 md:gap-10 lg:gap-20'>
                 {FEATURES.map(feature => 
                     <div className=' w-full rounded-[40px] px-4 py-6 bg-gray-[#f2f3f7] my-10 shadow-2xl'>
                         
